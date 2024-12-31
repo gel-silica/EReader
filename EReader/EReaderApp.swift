c4a9c8ea03bb126a5941771a507b2a93579e8526
silicagel <ksk19981004@gmail.com>
2024-12-31 22:40:40 +0900

    Initial Commit

---
+++ EReader/EReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EReaderApp.swift
+//  EReader
+//
+//  Created by silicagel on 2024/12/31.
+//
+
+import SwiftUI
+
+@main
+struct EReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
